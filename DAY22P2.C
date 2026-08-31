180e2b89f1179f42d44b0ad167ef7c9753123602
mohdsameermalik2007-cloud <mohdsameermalik2007@gmail.com>
2026-08-31 21:47:25 +0530

    added c program

---
+++ DAY22P2.C
@@ -0,0 +1,25 @@
+// Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    float sum = 0;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        if (i == 1)
+            sum = sum + 1;
+        else
+            sum = sum + (float)(2 * i - 1) / (2 * i);
+    }
+
+    printf("Sum of the series = %.2f", sum);
+
+    return 0;
+}
