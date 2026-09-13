d98c2fa045a20f0f847d5ba2a915126a491037b5
mohdsameermalik2007-cloud <mohdsameermalik2007@gmail.com>
2026-09-13 22:31:46 +0530

    added c program

---
+++ DAY35P1.C
@@ -0,0 +1,30 @@
+// Q69: Find the second largest element in an array.
+
+#include <stdio.h>
+
+int main() {
+    int a[100], n, largest, second;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    largest = second = -99999;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] > largest) {
+            second = largest;
+            largest = a[i];
+        }
+        else if (a[i] > second && a[i] != largest) {
+            second = a[i];
+        }
+    }
+
+    printf("Second largest = %d", second);
+
+    return 0;
+}
