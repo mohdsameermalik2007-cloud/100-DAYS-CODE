7136f555f5e0955cc8be7a2997db79654ef0b010
mohdsameermalik2007-cloud <mohdsameermalik2007@gmail.com>
2026-09-22 22:03:18 +0530

    added c program

---
+++ DAY44P2.C
@@ -0,0 +1,17 @@
+// Q88: Replace spaces with hyphens in a string.
+
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i;
+
+    printf("enter string: ");
+    gets(str);
+
+    for(i = 0; str[i] !='\0'; i++) {
+        if(str[i] == ' ')
+        str[i] ='-';
+    }
+    printf("new string: %s",str);
+    return 0;
+}
