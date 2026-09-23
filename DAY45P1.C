d9ff077273882961e5e0bba56355e46603d45ff1
mohdsameermalik2007-cloud <mohdsameermalik2007@gmail.com>
2026-09-23 23:11:48 +0530

    added c program

---
+++ DAY45P1.C
@@ -0,0 +1,25 @@
+//Q89. Count frequency of a given character in a string.
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100], ch;
+    int i = 0, count = 0;
+
+    fgets(str, sizeof(str), stdin);
+    scanf("%c", &ch);
+
+    while (str[i] != '\0')
+    {
+        if (str[i] == ch)
+        {
+            count++;
+        }
+        i++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
